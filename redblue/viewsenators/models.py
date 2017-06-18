e84ac5764001e3e556c37c7861019b9c186fcfd3
Armin Samii <asamii@adobe.com>
2017-06-18 13:28:18 -0700

    Move static logic into django template

--- redblue/viewsenators/models.py
+++ redblue/viewsenators/models.py
@@ -3,15 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class State(models.Model):
+    name = models.CharField(max_length=16)
+
 class Senator(models.Model):
     firstName = models.CharField(max_length=128)
     lastName = models.CharField(max_length=128)
     party = models.CharField(max_length=16)
-    state = models.ForeignKey(Senator, on_delete=models.CASCADE)
-
-class State(models.Model):
-    name = models.CharField(max_length=16)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
 
 class ContactList(models.Model):
-    senators = models.ManytoManyField(Senators)
+    senators = models.ManyToManyField(Senator)
--- redblue/viewsenators/views.py
+++ redblue/viewsenators/views.py
@@ -1,9 +1,59 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import json
+import os
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
-# Create your views here.
+Democrat = "D"
+Republican = "R"
+def _partyToText(whichParty):
+    if whichParty == Democrat:
+        return "Blue"
+    elif whichParty == Republican:
+        return "Red"
+    else:
+        assert False
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('viewsenators/index.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
+
+def createContactList(request):
+    whichParty = Republican
+
+    template = loader.get_template('viewsenators/index.html')
+    mocFn = os.path.join(settings.STATIC_ROOT, 'moc.json')
+
+    with open(mocFn, 'rb') as dataFile:
+        data = json.load(dataFile)
+    assert data['status'] == 'OK'
+
+    results = data['results']
+    assert len(results) == 1 # could have multiple congresses?
+    result = results[0]
+
+    assert result['congress'] == '115'
+    assert result['chamber'] == 'Senate'
+    assert result['offset'] == '0'
+
+    membersInParty = set()
+    for member in result['members']:
+        if member['party'] == whichParty:
+            membersInParty.add(member['state'])
+
+    partyText = _partyToText(whichParty)
+
+    # TODO generate senators list once
+    # TODO pass in data from the senators list
+    context = {
+        "text": partyText
+    }
+    return HttpResponse(template.render(context, request))
