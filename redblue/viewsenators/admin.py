bd7bf8bda7ad3151cc585d635e90d6c0c2160e3c
Armin Samii <armin@avametric.com>
2017-06-21 15:42:10 -0700

    Add admin site

--- redblue/viewsenators/admin.py
+++ redblue/viewsenators/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import ContactList
+
+@admin.register(ContactList)
+class ContactListAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
