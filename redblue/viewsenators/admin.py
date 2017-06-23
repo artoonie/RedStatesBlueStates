cb371674412ec2d33387ff259a50336c69802358
Armin Samii <armin@avametric.com>
2017-06-23 12:25:06 -0700

    Update admin to have textfield

--- redblue/viewsenators/admin.py
+++ redblue/viewsenators/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from .forms import ChooseForm
 from django.contrib import admin
 
 from .models import ContactList
@@ -8,3 +9,4 @@ from .models import ContactList
 @admin.register(ContactList)
 class ContactListAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+    form = ChooseForm
