c229a9ef8cf9d218c1cf01d5c1f8d9d8fe3eb169
Armin Samii <armin@avametric.com>
2017-06-23 12:40:10 -0700

    Create ChooseForSimple so senators don't show up in admin

--- redblue/viewsenators/admin.py
+++ redblue/viewsenators/admin.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from .forms import ChooseForm
+from django import forms
 from django.contrib import admin
 
 from .models import ContactList
 
+class ChooseFormSimple(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea())
+
 @admin.register(ContactList)
 class ContactListAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
-    form = ChooseForm
+    form = ChooseFormSimple
