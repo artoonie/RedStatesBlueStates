e209a96780d1889ba1f617ed0beb0b1a0ea72fa4
Armin Samii <armin@avametric.com>
2018-01-08 00:03:11 -0800

    Allow backwards migration

--- redblue/viewsenators/migrations/0004_add_cities.py
+++ redblue/viewsenators/migrations/0004_add_cities.py
@@ -116,10 +116,4 @@ class Migration(migrations.Migration):
             model_name='senator',
             name='party_old',
         ),
-
-        migrations.AddField(
-            model_name='state',
-            name='population',
-            field=models.PositiveIntegerField(default=0),
-        ),
     ]
